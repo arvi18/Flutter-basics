94a1d6fa700c1d8dad4985652ccd9e0082908f81
Arvind Shelke <ucdonor5@gmail.com>
2023-01-26 05:18:31 +0530

    first StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,38 @@
-// ignore_for_file: avoid_print
-import 'dart:developer';
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("My app"),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Text(
-              "afankf",
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 4.0,
-                color: Colors.grey,
-                fontFamily: "Raleway",
-              ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Text("click me"),
-            onPressed: () {},
-            hoverColor: Colors.red.shade100,
-            backgroundColor: Colors.green,
+      MaterialApp(home: TestWidget()),
+    );
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My app"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "text here",
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 4.0,
+            color: Colors.grey,
+            fontFamily: "Raleway",
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        hoverColor: Colors.red.shade100,
+        backgroundColor: Colors.green,
+        child: Text("click me"),
+      ),
     );
+  }
+}
